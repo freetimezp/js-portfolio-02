83ea00f20be4d76cc9d3a7aaccdcb05e2287a287
freetimezp <kraftukrnet@ukr.net>
2022-09-01 20:19:21 +0300

    09.Js-portfolio-02. Add some animate effect to Banner and Projects component.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,9 @@ import {ArrowRightCircle} from "react-bootstrap-icons";
 
 import HeaderImg from "../assets/img/header-img.svg";
 
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
@@ -46,17 +49,25 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm `}<span className="wrap">{text}</span></h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aspernatur cum eos maxime minus quasi repellat?
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aspernatur cum eos maxime minus quasi repellat?
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aspernatur cum eos maxime minus quasi repellat?
-                        </p>
-                        <button onClick={() => console.log('connect 2')}>Let's connect <ArrowRightCircle size={25} /></button>
+                        <TrackVisibility>
+                            {
+                                ({isVisible}) =>
+                                    <div className={isVisible ? "animate__animated animate__bounce" : ""}>
+                                        <span className="tagline">Welcome to my Portfolio</span>
+                                        <h1>{`Hi I'm `}<span className="wrap">{text}</span></h1>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Aspernatur cum eos maxime minus quasi repellat?
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Aspernatur cum eos maxime minus quasi repellat?
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Aspernatur cum eos maxime minus quasi repellat?
+                                        </p>
+                                    </div>
+                            }
+                        </TrackVisibility>
+                                    <button onClick={() => console.log('connect 2')}>Let's connect <ArrowRightCircle
+                                            size={25}/></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={HeaderImg} alt="Header"/>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,9 @@ import projImg3 from "../assets/img/project-img3.png";
 
 import colorSharp2 from "../assets/img/color-sharp2.png";
 
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
 export const Projects = () => {
     const projects = [
         {
@@ -46,44 +49,51 @@ export const Projects = () => {
             <Container>
                 <Row>
                     <Col>
-                        <h2>Projects</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Architecto assumenda at laborum quaerat quas sint!
-                        </p>
+                        <TrackVisibility>
+                            {
+                                ({isVisible}) =>
+                                    <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                                        <h2>Projects</h2>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Architecto assumenda at laborum quaerat quas sint!
+                                        </p>
+                                    </div>
+                            }
+                        </TrackVisibility>
 
-                        <Tab.Container id="projects-tab" defaultActiveKey="first">
-                            <Nav variant="pills" className="nav-pills mb-5 align-items-center justify-content-center" id="pills-tab">
-                                <Nav.Item>
-                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                            <Tab.Content>
-                                <Tab.Pane eventKey="first">
-                                    <Row>
-                                        {
-                                            projects.map((project, index) => {
-                                                return (
-                                                    <ProjectCard key={index} {...project} />
-                                                )
-                                            })
-                                        }
-                                    </Row>
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="second">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, nobis!
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="third">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, nobis!
-                                </Tab.Pane>
-                            </Tab.Content>
-                        </Tab.Container>
+                                    <Tab.Container id="projects-tab" defaultActiveKey="first">
+                                            <Nav variant="pills" className="nav-pills mb-5 align-items-center justify-content-center" id="pills-tab">
+                                                <Nav.Item>
+                                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                                </Nav.Item>
+                                                <Nav.Item>
+                                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                                </Nav.Item>
+                                                <Nav.Item>
+                                                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                                                </Nav.Item>
+                                            </Nav>
+                                            <Tab.Content>
+                                                <Tab.Pane eventKey="first">
+                                                    <Row>
+                                                        {
+                                                            projects.map((project, index) => {
+                                                                return (
+                                                                    <ProjectCard key={index} {...project} />
+                                                                )
+                                                            })
+                                                        }
+                                                    </Row>
+                                                </Tab.Pane>
+                                                <Tab.Pane eventKey="second">
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, nobis!
+                                                </Tab.Pane>
+                                                <Tab.Pane eventKey="third">
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, nobis!
+                                                </Tab.Pane>
+                                            </Tab.Content>
+                                        </Tab.Container>
                     </Col>
                 </Row>
             </Container>
